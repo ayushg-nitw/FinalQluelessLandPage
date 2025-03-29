1db2f6a94e97e338913aea9b22b3f1ccebeb8485
Ayush_Gupta <ayush7489716415@gmail.com>
2025-03-29 21:36:21 +0530

    hmr added in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    hmr:
+      process.env.NODE_ENV === "production"
+        ? false
+        : {
+            clientPort: 443,
+            host: "qlue.in",
+          },
+  },
   plugins: [react()],
   define: {
     "process.env": {}, // Allows environment variables
